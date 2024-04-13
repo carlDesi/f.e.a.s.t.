3191998f554b6ca45ae59982a5c3ff155e203a12
Zcada <139734723+Zcada@users.noreply.github.com>
2024-04-13 21:00:57 +0800

    Almost there guys

--- login/models.py
+++ login/models.py
@@ -7,6 +7,35 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, blank=True)
 
+    rooms = models.CharField(max_length=255, blank=True, help_text='Comma-separated list of room names')
+    adminRoom = models.JSONField(default=dict)
+
+    def add_room(self, room_name, student_score=0):
+        if not self.rooms:
+            self.rooms = room_name
+        else:
+            self.rooms += ',{}'.format(room_name)
+
+    def remove_room(self, room_name):
+        if self.rooms:
+            room_names = self.rooms.split(',')
+            room_names = [rn.strip() for rn in room_names if rn.strip()]
+            room_names = [rn for rn in room_names if rn != room_name]
+            self.rooms = ','.join(room_names)
+
+    def get_rooms(self):
+        if self.rooms:
+            return self.rooms.split(',')
+        else:
+            return []
+        
+    def get_admin_room(self):
+        return self.adminRoom
+    
+    def add_admin_room(self, room_name, room_uuid):
+        if room_name not in self.adminRoom:
+            self.adminRoom[room_uuid] = room_name
+
 def getUsers():
     users = User.objects.all()
     print ("Username : Password : Email : Status")
@@ -21,9 +50,15 @@ def deleteUsers():
 class Room(models.Model):
     name = models.CharField(max_length=255)
     code = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    students = models.JSONField(default=dict)
+    subrooms = models.JSONField(default=dict)
 
     def __str__(self):
         return self.name
+    
+    def delete_all_rooms():
+        rooms = Room.objects.all()
+        rooms.delete()
     
     @classmethod
     def create(cls, name, code=None):
@@ -33,19 +68,38 @@ class Room(models.Model):
         room.save()
         return room
     
+    def remove_student(self, student_name):
+        if student_name in self.students:
+            del self.students[student_name]
+            self.save()
+
+    def add_student_in_subroom(self, student_name, subroom_name):
+        if student_name not in self.subroom_name.values():
+            self.subrooms
+
+    def add_subroom(self, subroom_name):
+        if subroom_name not in self.subrooms:
+            self.subrooms[subroom_name] = []
+            self.save()
+
+    def delete_subroom(self, subroom_name):
+        if subroom_name in self.subrooms:
+            del self.subrooms[subroom_name]
+            self.save()
+    
 def getRooms():
     rooms = Room.objects.all()
     for room in rooms:
-        print(room.name + str(room.code))
+        print(room.name + " " + str(room.code) + " " + str(room.students))
 
 def getUserProfiles():
     users = UserProfile.objects.all()
     for user in users:
-        print("Username : Password : T/S")
-        print(str(user.user.username) + " : " + str(user.user.password) + " : " + str(user.status))
+        print("Username : Password : T/S : Student_Rooms : Teacher_Rooms")
+        print(str(user.user.username) + " : " + str(user.user.password) + " : " + str(user.status) + " : " + str(user.get_rooms()) + " : " + str(user.get_admin_room()))
 
-
-getUserProfiles()
+# getUserProfiles()
+getRooms()
 # deleteUsers()
 #to delete items from database:
 # # from login.models import Room
--- login/views.py
+++ login/views.py
@@ -63,29 +63,132 @@ def loginpage(request):
 def loginerror(request):
     return render(request, "loginerror.html")
 
-@login_required
+@login_required(login_url='/login/')
 def student_main(request):
     try:
         user_profile = UserProfile.objects.get(user=request.user, status='student')
-        return render(request, "student_main.html")
+        if request.method == "POST":
+            if 'class_code' in request.POST:
+                class_code = request.POST['class_code']
+                if Room.objects.filter(code=class_code).exists():
+                    room = Room.objects.get(code=class_code)
+                    if room.name not in user_profile.get_rooms():
+                        user_profile.add_room(room.name)
+                        user_profile.save()
+
+                        if not room.students:
+                            room.students = {}
+                        room.students[user_profile.user.email] = [0, []]
+                        room.save()
+
+                        # messages.success(request, "Room added successfully.")
+                        print("Room added successfully")
+                    else:
+                        messages.error(request, "Room with code '{}' is already in your list of rooms.".format(class_code))
+                        print("Room with code '{}' is already in your list of rooms.".format(class_code))
+                else:
+                    messages.error(request, "Room with code '{}' does not exist.".format(class_code))
+            elif 'remove_all_rooms' in request.POST:
+                #Roomname = request.POST['room_name_for_removal']
+                #theroom = Room.objects.get(code="ca94c44b-4dc0-43e8-864d-0a35c52395d8")
+                #theroom.remove_student(user_profile.user.email)
+                #theroom.save()
+                #print(theroom.students)
+                #user_profile.remove_room(Roomname)
+                #user_profile.save()
+                # Room.remove_student(Roomname, user_profile)
+                messages.success(request, "All rooms removed.")
+
+        user_rooms = user_profile.get_rooms()
+        user_rooms = list(set(user_rooms))
+        class_options = [('', '-- Select a class --')] + [(room, room) for room in user_rooms]
+
+        return render(request, "student_main.html", {'user_profile': user_profile, 'class_options': class_options})
         pass
     except UserProfile.DoesNotExist:
         return redirect("loginpage")
 
-@login_required
+@login_required(login_url='/login/')
 def teacher_main(request):
     try:
         user_profile = UserProfile.objects.get(user=request.user, status='teacher')
         if request.method == "POST":
-            title = request.POST['name']
-            newroom = Room.create(name=title)
-            return redirect("teacher_main")
-        else:
-            rooms = Room.objects.all()
-            return render(request, 'teacher_main.html', {'rooms': rooms})
-        pass
+            if 'delete_all_rooms' in request.POST:
+                print('Hello World')
+                user_profile.adminRoom = {}
+                user_profile.save()
+                Room.objects.all().delete()
+            elif 'get_info' in request.POST:
+                room_code = request.POST['get_info']
+                room = Room.objects.get(code=room_code)
+                students = room.students.items()
+                allsubrooms = room.subrooms.items()
+                rooms = Room.objects.filter(code__in=[str(code) for code in user_profile.get_admin_room().keys()])
+                return render(request, 'teacher_main.html', {'rooms':rooms, 'students': students, 'room':room, 'subrooms':allsubrooms})
+            elif 'room_name' in request.POST:
+                room_name = request.POST['room_name']
+                newroom = Room.create(name=room_name)
+                user_profile.add_admin_room(room_name, str(newroom.code))
+                user_profile.save()
+            elif 'add-sub-room-btn' in request.POST:
+                subroom_name = request.POST['subroom-name-input']
+                activeroomcode = request.POST['active-room-code']
+                activeroom = Room.objects.get(code=activeroomcode)
+                Room.add_subroom(activeroom, subroom_name)
+                allsubrooms = activeroom.subrooms.items()
+                room = Room.objects.get(code=activeroomcode)
+                students = room.students.items()
+                rooms = Room.objects.filter(code__in=[str(code) for code in user_profile.get_admin_room().keys()])
+                return render(request, 'teacher_main.html', {'rooms':rooms, 'students': students, 'room':room, 'subrooms':allsubrooms})
+            elif 'delete-sub-room-button' in request.POST:
+                if 'subroomname' in request.POST:    
+                    subroom_name = request.POST['subroomname']
+                    activeroomcode = request.POST['active-room-code']
+                    activeroom = Room.objects.get(code=activeroomcode)
+                    Room.delete_subroom(activeroom, subroom_name)
+                    allsubrooms = activeroom.subrooms.items()
+                    room = Room.objects.get(code=activeroomcode)
+                    students = room.students.items()
+                    rooms = Room.objects.filter(code__in=[str(code) for code in user_profile.get_admin_room().keys()])
+                    return render(request, 'teacher_main.html', {'rooms':rooms, 'students': students, 'room':room, 'subrooms':allsubrooms})
+            elif 'student-add-button' in request.POST:
+                if "student-name-input" in request.POST:
+                    student_name = request.POST['student-name-input']
+                    subroom_name = request.POST['subroomname']
+                    activeroomcode = request.POST['active-room-code']
+                    activeroom = Room.objects.get(code=activeroomcode)
+                    allsubrooms = activeroom.subrooms.items()
+                    room = Room.objects.get(code=activeroomcode)
+                    students = room.students.items()
+                    studentsinsubroom = room.subrooms[subroom_name]
+                    if student_name in room.students.keys():
+                        if room.students[student_name][1] == []:
+                            room.students[student_name][1].append(subroom_name)
+                            studentsinsubroom.append(student_name)
+                            room.save()
+                            activeroom = Room.objects.get(code=activeroomcode)
+                            allsubrooms = activeroom.subrooms.items()
+                    rooms = Room.objects.filter(code__in=[str(code) for code in user_profile.get_admin_room().keys()])
+                    return render(request, 'teacher_main.html', {'rooms':rooms, 'students': students, 'room':room, 'subrooms':allsubrooms})
+            elif 'student-remove-button' in request.POST:
+                student_name = request.POST['student_subroom_name']
+                subroom_name = request.POST['subroomname']
+                activeroomcode = request.POST['active-room-code']
+                activeroom = Room.objects.get(code=activeroomcode)
+                allsubrooms = activeroom.subrooms.items()
+                room = Room.objects.get(code=activeroomcode)
+                students = room.students.items()
+                studentsinsubroom = room.subrooms[subroom_name]
+                room.students[student_name][1].remove(subroom_name)
+                studentsinsubroom.remove(student_name)
+                room.save()
+                activeroom = Room.objects.get(code=activeroomcode)
+                allsubrooms = activeroom.subrooms.items()
+                rooms = Room.objects.filter(code__in=[str(code) for code in user_profile.get_admin_room().keys()])
+                return render(request, 'teacher_main.html', {'rooms':rooms, 'students': students, 'room':room, 'subrooms':allsubrooms})
+        rooms = Room.objects.filter(code__in=[str(code) for code in user_profile.get_admin_room().keys()])
+        return render(request, 'teacher_main.html', {'rooms':rooms})
     except UserProfile.DoesNotExist:
         return redirect("loginpage")
-
     # template = loader.get_template('login.html')
     # return HttpResponse(template.render())
